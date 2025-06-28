f08df6c316d54da3c4a35d0a861568f484e2707d
wendyww9 <wendyskystar@gmail.com>
2025-06-27 17:38:12 -0700

    Refactor NewBoardForm

--- src/components/NewBoardForm.jsx
+++ src/components/NewBoardForm.jsx
@@ -37,29 +37,25 @@ const NewBoardForm = ({ onCreateBoard }) => {
       [inputName]: inputValue,
     }));
   };
-    
+  const makeControlledInput = (inputName) => {
+    return <input 
+      onChange={handleChange} 
+      type='text' 
+      id={`input-${inputName}`}
+      name={inputName}
+      value={formData[inputName]}
+      className={errorData ? 'error' : ''}
+    />;
+  };
   return (
     <form className="new_board_form" onSubmit={handleSubmit}>
-    <label htmlFor="board_name">Board Name</label>
-    <input
-      type="text"
-      id="board_name"
-      name="title"
-      value={formData.title}
-      onChange={handleChange}
-      className={`error_input ${errorData ? 'error' : ''}`}
-    />
-    <label htmlFor="owner_name">Owner Name</label>
-    <input
-      type="text"
-      id="owner_name"
-      name="owner"
-      value={formData.owner}
-      onChange={handleChange}
-      className={`error_input ${errorData ? 'error' : ''}`}
-    />
+      <label htmlFor="board_name">Board Name</label>
+      { makeControlledInput('title') }
 
-      {errorData && <div className="error_message">{errorData}</div>}
+      <label htmlFor="owner_name">Owner Name</label>
+      { makeControlledInput('owner') }
+      
+      { errorData && <div className="error_message">{errorData}</div> }
       <button type="submit" className="create_board_button">Create Board</button>
     </form>
 );
